943a338c214f3000da84577adc6fecec88edf952
Prateek Rohilla <prateek.rohila@netomi.com>
2021-12-07 14:01:42 +0530

    fix dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,30 +1,55 @@
 <template>
   <div>
-    <el-button class="min-w-full hover:bg-purple-100 hover:border-purple-800" @click="dialogVisible = true"
-      >
-      <div class="flex ai-center">
-            <el-avatar :src="person.picture.thumbnail"></el-avatar>
-            <h3>
-              <strong class="m-l-2">
-                {{ person.name.first }} {{ person.name.middle }}
-                {{ person.name.last }}
-              </strong>
-              <!-- ({{ person.dob.age }} {{ person.gender[0].toUpperCase() }}) -->
-            </h3>
-          </div>
-      
-      </el-button
+    <el-button
+      class="min-w-full hover:bg-purple-100 hover:border-purple-800"
+      @click="dialogVisible = true"
     >
+      <div class="flex ai-center">
+        <el-avatar :src="person.picture.thumbnail"></el-avatar>
+        <h3>
+          <strong class="m-l-2">
+            {{ person.name.first }} {{ person.name.middle }}
+            {{ person.name.last }}
+          </strong>
+          <!-- ({{ person.dob.age }} {{ person.gender[0].toUpperCase() }}) -->
+        </h3>
+      </div>
+    </el-button>
 
     <el-dialog
-    class="hover:rounded-xl"
+      class="hover:rounded-xl"
       :title="fullName"
       :visible.sync="dialogVisible"
       width="50%"
       :before-close="handleClose"
     >
-      <span>This is a message</span>
+      <span>
+        <div
+          class="
+            border-2
+            rounded-lg
+            transition-4 transition
+            duration-500
+            flex
+            hover:border-indigo-400
+          "
+          :body-style="{ padding: '0px' }"
+        >
+          <img
+            :src="person.picture.large"
+            class="rounded-xl m-3 shadow-md border-gray-300 border-4"
+          />
+          <div style="padding: 14px">
+            <p class="font-medium text-left">{{ fullName }}</p>
+            <p class="font-medium text-left">{{ person.email }}</p>
+            <div class="bottom clearfix">
+              <time class="time">{{ person.dob.date }}</time>
+            </div>
+          </div>
+        </div>
+      </span>
       <span slot="footer" class="dialog-footer">
+        <el-button type="success" class="button">Operating</el-button>
         <el-button type="secondary" @click="dialogVisible = false"
           >Close</el-button
         >
@@ -47,7 +72,7 @@ export default {
   },
   methods: {
     handleClose() {
-      return this.dialogVisible = false;
+      return (this.dialogVisible = false);
     },
   },
   computed: {
@@ -58,5 +83,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
+.thumbnail {
+  width: 40px;
+  height: 40px;
+  border-radius: 4px;
+}
 </style>
